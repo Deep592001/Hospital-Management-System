4579e662ae09f2d4b80415fdbb39facb8a6f80a7
Deep592001 <72622330+Deep592001@users.noreply.github.com>
2020-10-09 22:40:44 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,25 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col2">
+  <div id="gallery">
+    <ul>
+      <li class="placeholder" style="background-image:url(images/demo/image1.jpg);">Image Holder</li>
+      <li><a class="swap" style="background-image:url(images/demo/img3.jpg);" href="#gallery"><strong>Services</strong><span><img src="images/demo/image1.jpg" alt="" /></span></a></li>
+      <li><a class="swap" style="background-image:url(images/demo/img1.jpg);" href="#gallery"><strong>Products</strong><span><img src="images/demo/shr_290913_hospital2.jpg" alt="" /></span></a></li>
+      <li class="last"><a class="swap" style="background-image:url(images/demo/img2.jpg);" href="#gallery"><strong>Company</strong><span><img src="images/demo/shr_290913_hospital5.jpg" alt="" /></span></a></li>
+    </ul>
+    <div class="clear"></div>
+  </div>
+</div>
+<div class="wrapper col4">
+  <div id="container">
+    <div id="content">
+         </div>
+    
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+include("header.php");
+?>
+<div class="wrapper col1">
+  <div id="container">
+    <div id="content">
+      <h1>Welcome to Hospital Management System</h1>
+      <p>  The goal of the Doctor appointment SYSTEM
+  System is to improve the efficiency of the health by reducing the overall time and cost used to create documents and retrieve information.
+.</p>
+      <p>The main feature of E-Knowledge in Doctor appointment system is to provide the browser to get appointments from a doctor through internet instead of going there and fixing an appointment..</p>
+      <p>	Everyone needs to have Medical attention at any time. So we allow every user to register freely at any time..</p>
+      <p>Doctor appointment System maintains patient’s prescriptions so that their medical details are always available in Internet, which will be more convenient for the patients. This will be more comfortable for the patient..</p>
+
+      <div class="homecontent">
+        
+        <div class="clear"></div>
+      </div>
+    </div>
+   
+    <div class="clear"></div>
+  </div>
+</div>
+<?php
+include("footer.php");
+?>
